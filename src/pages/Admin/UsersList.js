a2b19d1fa23432d62ffb5571c4a34dd856edaa3e
korone8282 <korone8282@gmail.com>
2024-04-07 17:50:23 +0530

    update2

--- src/pages/Admin/UsersList.js
+++ src/pages/Admin/UsersList.js
@@ -4,11 +4,14 @@ import { USERINFO_URL } from '../../redux/Utils/constants';
 import { FaRegEdit } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Loader from '../../components/Loader';
 
 const UserList = () => {
 
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(1);
 
   const {userinfo} = useSelector(state=>state.auth);
   
@@ -18,6 +21,8 @@ const UserList = () => {
       try {
         const res = await apiConnector(USERINFO_URL,"GET",null,{Authorization: `Bearer ${userinfo.token}`});
         setUsers(res.data.users);
+
+        setLoading(0);
       } catch (error) {
         console.log(error);
       }
@@ -32,8 +37,10 @@ const UserList = () => {
  
   return (
     <div className='text-3xl flex justify-center mt-12'>
-
-<table className=' border-2 border-black'>
+  {
+    loading ? (<Loader/>
+    ) : (
+      <table className=' border-2 border-black'>
 <thead>
 <tr className='border-2 text-start'>
    <th className='border-2 w-48 p-2'>S No.</th>
@@ -61,6 +68,9 @@ const UserList = () => {
      
 
 </table>
+
+    ) 
+  }
 
     </div>
 
