879212687879f347b9bb4618ff9394c744632b8d
korone8282 <korone8282@gmail.com>
2024-04-07 06:02:54 +0530

    update2

--- src/redux/Utils/apiConnector.js
+++ src/redux/Utils/apiConnector.js
@@ -5,8 +5,6 @@ export const axiosInstance = axios.create({});
 export const apiConnector = (url,method, bodyData, headers, params) => {
   return axiosInstance({
     method: `${method}`,
-    withCredentials: true,
-    credentials: 'include',
     url: `${url}`,
     data: bodyData ? bodyData : null,
     headers: headers ? headers : null,
