f166c3eea600cc850763dc0f94396f0e2cf2080c
korone8282 <korone8282@gmail.com>
2024-04-10 23:38:31 +0530

    update

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 exports.authorization = async(req,res,next)=>{
 try{
                            
-    const token =  req.header("Authorization").replace("Bearer ", "") || req.cookies.cookie1;
+    const token =  req.header("Authorization").replace("Bearer ", "");
 
     if(!token){
         res.status(401).json({
