c397c7ccfe33cc7039f81754fbcd5afc1e4ff27b
korone8282 <korone8282@gmail.com>
2025-04-01 00:43:27 +0530

    ultrae

--- src/redux/Utils/constants.js
+++ src/redux/Utils/constants.js
@@ -1,34 +1,34 @@
-export const BASE_URL = 'http://alimentsglobal.onrender.com'
+export const BASE_URL = 'https://alimentsglobal.onrender.com'
 
-export const LOGIN_API = 'http://alimentsglobal.onrender.com/api/users/login'
-export const REGISTER_API = 'http://alimentsglobal.onrender.com/api/users/signup'
-export const LOGOUT_API = 'http://alimentsglobal.onrender.com/api/users/logout'
+export const LOGIN_API = 'https://alimentsglobal.onrender.com/api/users/login'
+export const REGISTER_API = 'https://alimentsglobal.onrender.com/api/users/signup'
+export const LOGOUT_API = 'https://alimentsglobal.onrender.com/api/users/logout'
 
-export const GOOGLE_API = 'http://alimentsglobal.onrender.com/api/users/googleLogin'
+export const GOOGLE_API = 'https://alimentsglobal.onrender.com/api/users/googleLogin'
 export const CLIENT_ID = "922549263546-0jat4nh1jrjt07a58nhsm469vdsvhdbb.apps.googleusercontent.com"
 
-export const OTPLOG_URL = 'http://alimentsglobal.onrender.com/api/users/otpLogin'
-export const GENOTP_URL = 'http://alimentsglobal.onrender.com/api/users/otpEntry'
-export const VERIFY_URL = 'http://alimentsglobal.onrender.com/api/users/verifyOtp'
+export const OTPLOG_URL = 'https://alimentsglobal.onrender.com/api/users/otpLogin'
+export const GENOTP_URL = 'https://alimentsglobal.onrender.com/api/users/otpEntry'
+export const VERIFY_URL = 'https://alimentsglobal.onrender.com/api/users/verifyOtp'
 
-export const PROFILE_URL = 'http://alimentsglobal.onrender.com/api/users/profile'
-export const USERINFO_URL = 'http://alimentsglobal.onrender.com/api/users/getAllUsers'
-export const USERID_URL = 'http://alimentsglobal.onrender.com/api/users/'
+export const PROFILE_URL = 'https://alimentsglobal.onrender.com/api/users/profile'
+export const USERINFO_URL = 'https://alimentsglobal.onrender.com/api/users/getAllUsers'
+export const USERID_URL = 'https://alimentsglobal.onrender.com/api/users/'
 
-export const UPLOAD_URL = 'http://alimentsglobal.onrender.com/api/uploads'
+export const UPLOAD_URL = 'https://alimentsglobal.onrender.com/api/uploads'
 
-export const CATEGORIES_URL = 'http://alimentsglobal.onrender.com/api/category'
+export const CATEGORIES_URL = 'https://alimentsglobal.onrender.com/api/category'
 
-export const PRODUCT_URL = 'http://alimentsglobal.onrender.com/api/product'
+export const PRODUCT_URL = 'https://alimentsglobal.onrender.com/api/product'
 
-export const GOAL_URL = 'http://alimentsglobal.onrender.com/api/goal'
+export const GOAL_URL = 'https://alimentsglobal.onrender.com/api/goal'
 
-export const DATA_URL = 'http://alimentsglobal.onrender.com/api/data'
+export const DATA_URL = 'https://alimentsglobal.onrender.com/api/data'
 
-export const STORE_URL = 'http://alimentsglobal.onrender.com/api/inventory'
+export const STORE_URL = 'https://alimentsglobal.onrender.com/api/inventory'
 
-export const DISPATCH_URL = 'http://alimentsglobal.onrender.com/api/dispatch'
+export const DISPATCH_URL = 'https://alimentsglobal.onrender.com/api/dispatch'
 
-export const EXPORT_URL = 'http://alimentsglobal.onrender.com/api/export'
+export const EXPORT_URL = 'https://alimentsglobal.onrender.com/api/export'
 
-export const GRAPH_URL = 'http://alimentsglobal.onrender.com/api/graph'
+export const GRAPH_URL = 'https://alimentsglobal.onrender.com/api/graph'
