d24528f61e1dbbb90918e9140937839f5bedc517
yoyokorone <sangeeta.saxena6724@gmail.com>
2025-07-05 13:12:29 +0530

    firstyy

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
-	})
+	}) 
 );
 app.use(express.urlencoded({ extended: true }));
 
